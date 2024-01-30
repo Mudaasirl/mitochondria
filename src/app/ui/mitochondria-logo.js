997154b5cba14c6b212f4349fa17ef6fa9406e5c
Mudaasirl <shabanab488@gmail.com>
2024-01-30 21:47:42 +0530

    added definitions

---
+++ src/app/ui/mitochondria-logo.js
@@ -0,0 +1,13 @@
+ import { BeakerIcon } from '@heroicons/react/24/outline';
+
+
+export default function Mitochondrialogo() {
+  return (
+    <div
+      className={` flex flex-row items-center leading-none text-white`}
+    >
+      <BeakerIcon className="h-21 w-21 rotate-[15deg]" />
+      <p className="text-[22px]">Mitochondria</p>
+    </div>
+  );
+}
